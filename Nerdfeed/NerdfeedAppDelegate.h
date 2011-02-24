c8af9f50665b7ddc2eb0050fa5c44769dc1a7b7b
James Haley II <jhaley@j2-macbook-2.local>
2011-02-24 16:03:27 -0500

    Initial Commit

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by James Haley II on 2/24/11.
+//  Copyright 2011 J 2 Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by James Haley II on 2/24/11.
+//  Copyright 2011 J 2 Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
